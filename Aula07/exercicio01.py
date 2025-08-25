0c223021f3c328626ab1bfff6366b123594d9b68
Kassyani <224636135+Kassyani@users.noreply.github.com>
2025-08-25 15:51:44 -0300

    atualizado em sala

--- Aula07/exercicio01.py
+++ Aula07/exercicio01.py
@@ -11,13 +11,13 @@ while True:
    lanche = input("Digite o nome do lanche: ")
    if lanche == "hamburguer":
        print(f"Pedido confirmado")
-       break
+       
    else:
        print("Lanche não disponível. Tente novamente!")
 
-   cupom = input("Digite um cupom de desconto: ")
-   if cupom == codigo_de_desconto:
-     print(f"Seu lanche com desconto R$ {hamburguer - desconto}")
-   else:
-       print(f"Valor do hambúrguer sem desconto é: R$ {hamburguer}")
-       
+    cupom = input("Digite um cupom de desconto: ")
+    if cupom == codigo_de_desconto:
+        print(f"Seu lanche com desconto R$ {hamburguer - desconto}")
+    else:
+        print(f"Valor do hambúrguer sem desconto é: R$ {hamburguer}")
+    break
