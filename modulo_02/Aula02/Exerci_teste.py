2032f32fc302aa3cdaf7f2346bd1e9144e9d1845
Kassyani Mênedy Faria <224636135+Kassyani@users.noreply.github.com>
2025-10-09 09:27:20 -0300

    exercicios de casa

--- modulo_02/Aula02/Exerci_teste.py
+++ modulo_02/Aula02/Exerci_teste.py
@@ -1,6 +1,11 @@
-dados_usuario = {
+'''dados_usuario = {
     'nome': 'Kassyani', 
     'idade':'24 anos', 
     'cidade':'Maricá'}
 for dado in dados_usuario:
-    print(f'As informações que eu tenho são:{dado}')
+    print(f'As informações que eu tenho são:{dado}')'''
+
+def saudação() -> str:
+    print('Olá')
+    
+saudação()
