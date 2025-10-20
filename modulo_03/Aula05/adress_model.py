bd9ca69bb47c253b43baf75ad0c648e420608aa5
Kassyani <224636135+Kassyani@users.noreply.github.com>
2025-10-20 15:36:33 -0300

    exercicios de dbd

---
+++ modulo_03/Aula05/adress_model.py
@@ -0,0 +1,7 @@
+'''
+crie a tabela de endereços,
+a tabela deve fazer relação com tabela alunos
+um endereço pode ter muitos alunos.
+liste todos os alunos de um endereço pelo id do endereço.
+a tabela endereço terás a coluna id e local (pelo menos)
+'''
