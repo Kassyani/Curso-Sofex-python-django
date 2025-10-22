54fa1b6c923fd2dfd5c9d546f1dbdc5d98a33477
Thiago Pereira <224636135+Kassyani@users.noreply.github.com>
2025-10-22 15:38:33 -0300

    Atualizações de atividades

---
+++ modulo_03/Aula01/ati.py
@@ -0,0 +1,1 @@
+#Procurar pdf da atividade
--- modulo_03/Aula05/adress_model.py
+++ modulo_03/Aula05/adress_model.py
@@ -1,7 +1,8 @@
-'''
-crie a tabela de endereços,
-a tabela deve fazer relação com tabela alunos
-um endereço pode ter muitos alunos.
+"""
+crie a tabela de endereços;
+a tabela deve fazer relação com a tabela alunos
+um endereço poder ter muitos alunos,
+mas cada aluno pode ter apenas um endereço.
 liste todos os alunos de um endereço pelo id do endereço.
-a tabela endereço terás a coluna id e local (pelo menos)
-'''
+a tabela endereço terá a coluna id e local (pelo menos)
+"""
